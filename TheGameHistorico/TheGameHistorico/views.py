01ee994a3f1a33db0bb324253322e4ac6c35c991
Felipe Holanda Bezerra <felipe.h.bezerra@gmail.com>
2021-11-19 19:49:15 -0300

    Movendo projeto para o Eclipse

---
+++ TheGameHistorico/TheGameHistorico/views.py
@@ -0,0 +1,16 @@
+'''
+Created on Nov 19, 2021
+
+@author: felipe
+'''
+from django.shortcuts import redirect, render 
+from django.http import HttpResponse
+# from TheGameHistorico.forms import NewUser
+
+
+ 
+# Create your views here. 
+ 
+def home(request): 
+    return render(request, 'index.html')
+    # return HttpResponse('Alô mundo!')
